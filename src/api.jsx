beb43098f445cecf29f241190c6ea3e956f3fc6f
tonkyou <chadaporn.ssw@gmail.com>
2025-04-15 09:31:26 +0700

    update port https

--- src/api.jsx
+++ src/api.jsx
@@ -5,7 +5,7 @@ const api = axios.create({
   // baseURL: 'http://localhost:3005',
   // baseURL: 'https://203.150.243.10:8000',
   baseURL: 'https://api.comenspu.com',
-  timeout: 5000,
+  timeout: 10000,
 });
 
 export default api;
