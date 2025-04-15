ba6c573ae45750ac853ce19df25d4ede615e3a74
tonkyou <chadaporn.ssw@gmail.com>
2025-04-15 09:44:46 +0700

    update port https

--- src/api.jsx
+++ src/api.jsx
@@ -1,28 +1,15 @@
-// // src/api.jsx
-// import axios from "axios";
-// import https from "https"
-
-// const api = axios.create({
-//   // baseURL: 'http://localhost:3005',
-//   baseURL: 'https://203.150.243.10:8000',
-//   // baseURL: 'https://api.comenspu.com',
-//   timeout: 10000,
-//   httpsAgent: new https.Agent({  
-//     rejectUnauthorized: false
-//   })
-// });
-
-// export default api;
-
+// src/api.jsx
 import axios from "axios";
-import https from "https";
+import https from "https"
 
 const api = axios.create({
-  baseURL: 'https://203.150.243.10:8000',
+  // baseURL: 'http://localhost:3005',
+  // baseURL: 'https://203.150.243.10:8000',
+  baseURL: 'https://api.comenspu.com',
   timeout: 10000,
-  httpsAgent: new https.Agent({  
-    rejectUnauthorized: false
-  })
+  // httpsAgent: new https.Agent({  
+  //   rejectUnauthorized: false
+  // })
 });
 
 export default api;
